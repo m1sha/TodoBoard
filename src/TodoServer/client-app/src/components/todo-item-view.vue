acf6e8a39ba53f21d0d57f4c242e3d7afc920d36
m1sha <newsbag@live.ru>
2020-02-26 16:32:47 +0700

    Fix. UI

--- src/TodoServer/client-app/src/components/todo-item-view.vue
+++ src/TodoServer/client-app/src/components/todo-item-view.vue
@@ -3,10 +3,24 @@
     <div class="todo-column" style="flex:1">
       <h3> {{item.title }}</h3>
       <code>{{item.message}}</code>
+      <div>
+        <span class="todo-column-key">by</span>
+        <span class="todo-column-value">{{ item.userCreator.name }}</span>
+        <span class="todo-column-key">to</span>
+        <span class="todo-column-value">{{ item.userCreator.uid === item.userAssign.uid ? "self" : item.userAssign.name }}</span>
+      </div>
+      <div>
+        <span class="todo-column-key">created</span>
+        <span class="todo-column-value" :title="createDate">{{ timeAgo.format( new Date(item.createDate)) }}</span>
+        <template v-if="item.createDate!=item.changeDate">
+          <span class="todo-column-key">changed</span>
+          <span class="todo-column-value" :title="createDate">{{ timeAgo.format( new Date(item.changeDate)) }}</span>
+        </template>
+      </div>
     </div>
-    <div class="todo-column">
-      <button class="btn btn-outline-secondary padding" @click="onEdit(item)">Edit</button>
-      <button class="btn btn-outline-secondary padding" @click="onRemove(item)">Remove</button>
+    <div class="todo-commands">
+      <button class="btn btn-outline-secondary padding" @click="onEdit(item)"><i class="fa fa-pen"></i></button>
+      <button class="btn btn-outline-secondary padding" @click="onRemove(item)"><i class="fa fa-trash"></i></button>
     </div>
     
   </div>
@@ -26,6 +40,16 @@
   flex-direction: column;
 }
 
+.todo-commands{
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+
+.todo-commands button i {
+  padding: 4px 0;
+}
+
 h3{
   color: $text-color;
   font-size: 12pt;
@@ -33,19 +57,40 @@ h3{
 
 code{
   color: $text-color-second;
+  padding: 4px 0;
+  white-space: pre-line;
 }
 
 .padding{
   margin: 2px;
 }
 
+.todo-column-key{
+ color: $text-color-second;
+ font-size: 9pt;
+}
+
+.todo-column-value{
+  font-size: 9pt;
+  font-weight: 500;
+  color: $text-color;
+  padding: 0 4px;
+}
+
 </style>
 <script lang="ts">
 import Vue from 'vue'
 import { TodoItem } from '@/entities/todo-item'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
 export default Vue.extend({
   props:{
-    item : TodoItem
+    item : TodoItem,
+  },
+  data(){
+    return {
+      timeAgo: null
+    }
   },
   methods:{
     onEdit(item : TodoItem){
@@ -53,7 +98,23 @@ export default Vue.extend({
     },
     onRemove(item : TodoItem){
       this.$emit("remove", item)
+    },
+    
+ 
+  },
+  computed:{
+    createDate(){
+      return new Date(this.item.createDate).toLocaleString()
+    },
+    changeDate(){
+      return new Date(this.item.changeDate).toLocaleString()
     }
+  },
+  mounted(){
+    TimeAgo.addLocale(en)
+    this.timeAgo = new TimeAgo('en-US')
   }
 })
+
+
 </script>
